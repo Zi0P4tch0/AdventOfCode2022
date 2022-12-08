84af35548d8c3aabad9a7a123d5f5342b7444d20
Matteo Pacini <m+github@matteopacini.me>
2022-12-08 01:38:02 +0000

    Restricted valgrind-glib.sh to Linux only

--- valgrind-glib.sh
+++ valgrind-glib.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+[ "$(uname)" != "Linux" ] && {
+    echo "This script is for Linux only"
+    exit 1
+}
+
 export GC_DEBUG=gc-friendly
 export GC_SLICE=always-malloc
 valgrind --tool=memcheck --leak-check=full --num-callers=20 "$@"
